193f01a72055304f16760d8b67a0852f3d96dc17
Harwin <sandhuharwinkaur@gmail.com>
2025-09-22 23:01:48 +1000

    finsihed the UI part

---
+++ script.js
@@ -0,0 +1,13 @@
+// slide to go to app screen    
+
+const slider = document.getElementById("mySlider");
+
+slider.addEventListener("change", (e) => {
+    const {value} = e.target;
+    const label = 
+    if(value > 70){
+        
+    }else{
+
+    }
+});
